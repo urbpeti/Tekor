8fb7a30143ee3f248d8f7f5a28a33777731d704e
Urbanovics Peter <urbpeti@gmail.com>
2017-11-25 00:16:33 +0100

    GoalList

---
+++ Tekor/Controllers/GoalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tekor.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tekor.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class GoalsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public GoalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            List<Goal> test = _context.Goal.Include(x=> x.Reward).ToList();
+            List<object> goalItems = test.Select(x => new { x.Description ,x.Reward.Name }).ToList<object>();
+
+            //List<string> test = new List<string> { "siker", "siker2", "siker3" };
+            return Ok(new { goalItems = goalItems});
+        }
+    }
+}
